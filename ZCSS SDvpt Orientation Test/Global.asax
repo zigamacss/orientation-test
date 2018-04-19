f5752df213575688731208fc7623bb09bc165d78
zigamacss <38527088+zigamacss@users.noreply.github.com>
2018-04-19 18:09:55 +0200

    Add more features

---
+++ ZCSS SDvpt Orientation Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZCSS_SDvpt_Orientation_Test.MvcApplication" Language="C#" %>
---
+++ ZCSS SDvpt Orientation Test/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZCSS_SDvpt_Orientation_Test.Startup))]
+namespace ZCSS_SDvpt_Orientation_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+        //Application Guide
+    }
+}
